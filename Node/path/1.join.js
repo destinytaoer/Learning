13dacb842a250eb0aa5f0434dc26e3a103e51340
destinytaoer <1848765519@qq.com>
2019-08-12 16:54:13 +0800

    [node] 重新学习 path.join 方法

--- Node/path/1.join.js
+++ Node/path/1.join.js
@@ -4,3 +4,5 @@ const path = require('path');
 console.log(path.join('a\\', '\\b'));
 // 使用 \ 或 / 连接两个目录
 // 由于操作系统可能不同，目录的连接符可能不一样，所以不能直接使用字符串拼接的方式，而是使用 join 方法
+console.log(path.join(__dirname, './a'));
+console.log(path.join('./a', '..', './b'));
