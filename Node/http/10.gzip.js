2d1ba1beece115278600892538dc16b2e623261d
destinytaoer <1848765519@qq.com>
2019-08-30 16:00:03 +0800

    [node] 使用压缩方法进行压缩

---
+++ Node/http/10.gzip.js
@@ -0,0 +1,9 @@
+// 不是文件的压缩
+let zlib = require('zlib');
+let str = 'hello';
+zlib.gzip(str, (err, buffer) => {
+  console.log(buffer.length);
+  zlib.unzip(buffer, (err, data) => {
+    console.log(data.toString());
+  });
+});
