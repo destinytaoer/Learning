1a6b7cd8872e30f314a8f4c3c95147d34d6c32ed
destinytaoer <1848765519@qq.com>
2019-09-11 19:48:33 +0800

    [node] 学习 express 中间件的使用和原理

---
+++ Node/express/2.middleware.js
@@ -0,0 +1,21 @@
+const express = require('./express');
+const app = express();
+
+// 使用 use 定义一个中间件，next 是一个函数，调用它意味着当前中间件执行完毕，可以继续往下执行别的中间件或路由匹配了
+// 省略路径时，就是 / 匹配所有路径
+app.use(function(req, res, next) {
+  console.log('没有路径的中间件');
+  next();
+});
+
+app.use('/water', function(req, res, next) {
+  console.log('过滤 water');
+  next();
+});
+
+app.get('/water', function(req, res) {
+  res.end('water');
+});
+app.listen(8080, function() {
+  console.log('server started at 8080');
+});
--- Node/express/express.js
+++ Node/express/express.js
@@ -7,16 +7,33 @@ function createApplication() {
     // 遍历路由规则进行匹配，成功就执行回调
     let method = req.method;
     let { pathname } = url.parse(req.url, true);
-    for (let i = 0; i < app.routes.length; i++) {
-      let route = app.routes[i];
-      if (
-        (route.method === method.toLowerCase() || route.method === 'all') &&
-        (route.path === pathname || route.path === '*')
-      ) {
-        return route.handler(req, res);
+    let index = 0;
+    ~(function next() {
+      if (index >= app.routes.length) {
+        return res.end(`Cannot ${method} ${pathname}`);
       }
-    }
-    res.end(`Cannot ${method} ${pathname}`);
+      let route = app.routes[index++];
+      if (route.method === 'middle') {
+        // 只要请求路径是以中间件路径开头即可，加 / 为了使 /user 不能匹配 /users
+        if (
+          route.name === '/' ||
+          pathname.startsWith(route.path + '/') ||
+          pathname === route.name
+        ) {
+          route.handler(req, res, next);
+        } else {
+          next();
+        }
+      } else {
+        // 路由
+        if (
+          (route.method === method.toLowerCase() || route.method === 'all') &&
+          (route.path === pathname || route.path === '*')
+        ) {
+          return route.handler(req, res);
+        }
+      }
+    })();
   };
   app.listen = function() {
     let server = http.createServer(app);
@@ -39,6 +56,18 @@ function createApplication() {
   app.all = function(path, handler) {
     app.routes.push({
       method: 'all',
+      path,
+      handler
+    });
+  };
+
+  app.use = function(path, handler) {
+    if (typeof path === 'function') {
+      handler = path;
+      path = '/';
+    }
+    app.routes.push({
+      method: 'middle',
       path,
       handler
     });
