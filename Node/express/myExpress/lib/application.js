738d032cf7a330cac66310bfbdabc087c94f48e2
destinytaoer <1848765519@qq.com>
2019-09-19 17:03:23 +0800

    [node] 手写 express：增加 app 的 get 和 set 功能

--- Node/express/myExpress/lib/application.js
+++ Node/express/myExpress/lib/application.js
@@ -5,15 +5,29 @@ const methods = require('methods'); // 返回所有方法字符串的数组
 // http.METHODS 都是大写，而 methods 中都是小写
 // 用于将类数组转换为数组
 const slice = Array.prototype.slice;
-function Application() {}
+function Application() {
+  this.settings = {};
+}
 // 延迟初始化 Router，只有当需要的时候再进行 Router 的初始化，节省性能
 Application.prototype.lazyrouter = function() {
   if (!this._router) {
     this._router = Router();
   }
 };
+
+// 传二个参数表示设置，传一个参数表示获取
+Application.prototype.set = function(key, val) {
+  if (arguments.length == 1) {
+    return this.settings[key];
+  }
+  this.settings[key] = val;
+};
 methods.forEach(function(method) {
   Application.prototype[method] = function() {
+    // 额外的 get 功能，获取 app 中设置的值
+    if (method == 'get' && arguments.length == 1) {
+      return this.set(arguments[0]);
+    }
     // 使用到时，才进行初始化
     this.lazyrouter();
     this._router.get.apply(this._router, slice.call(arguments));
