cd909d45deb978494bde85f8ffec2619bcf6cf47
destinytaoer <1848765519@qq.com>
2019-09-19 17:57:32 +0800

    [node] 手写 express：增强 req 和 res

--- Node/express/myExpress/lib/middle/init.js
+++ Node/express/myExpress/lib/middle/init.js
@@ -1,4 +1,10 @@
-function enhanceReq(req) {}
+const url = require('url');
+function enhanceReq(req) {
+  let { pathname, query } = url.parse(req.url, true);
+  req.path = pathname;
+  req.query = query;
+  req.hostname = req.headers['host'].split(':')[0];
+}
 function enhanceRes(res) {
   res.render = function(filepath, options, callback) {
     function done(err, html) {
@@ -6,6 +12,24 @@ function enhanceRes(res) {
       res.end(html);
     }
     res.app.render(filepath, options, callback || done);
+  };
+  res.json = function(obj) {
+    res.setHeader('Content-Type', 'application/json');
+    const str = JSON.stringify(obj);
+    res.end(str);
+  };
+  res.send = function(msg) {
+    let type = typeof msg;
+    if (type == 'object') {
+      res.json(msg);
+    } else if (type == 'number') {
+      res.setHeader('Content-Type', 'application/plain');
+      res.status(msg);
+      res.end(http.STATUS_CODES[msg]);
+    } else {
+      res.setHeader('Content-Type', 'application/html');
+      res.end(msg);
+    }
   };
 }
 function init(req, res, next) {
