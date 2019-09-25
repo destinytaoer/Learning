01c2fe2afdf556ba5fe160406ed00ea737a8c3b4
destinytaoer <1848765519@qq.com>
2019-09-25 16:35:53 +0800

    [node] 手写 koa：重构代码，实现异步处理

--- Node/koa/myKoa/koa.js
+++ Node/koa/myKoa/koa.js
@@ -5,25 +5,54 @@ class Koa {
   }
   use(fn) {
     this.middleware.push(fn);
+    return this;
+  }
+  compose(middleware, ctx) {
+    return function fnMiddleware() {
+      function dispatch(i) {
+        let fn = middleware[i];
+        if (!fn) return Promise.resolve(); // 终止条件
+        return Promise.resolve(
+          // 返回一个 Promise
+          fn(ctx, function next() {
+            dispatch(i + 1);
+          })
+        );
+      }
+      return dispatch(0);
+    };
+  }
+  handleResponse(ctx) {
+    let body = ctx.body;
+    let res = ctx.res;
+    if (!body) {
+      res.statusCode = 404;
+      res.end('Not Found');
+    }
+    if (typeof body === 'string') {
+      ctx.type = 'text';
+      res.end(body);
+    }
+    //...
+  }
+  callback(req, res) {
+    let ctx = { req, res };
+    let fnMiddleware = this.compose(
+      this.middleware,
+      ctx
+    );
+    fnMiddleware()
+      .then(() => {
+        // 走完所有中间件之后
+        this.handleResponse(ctx);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   listen(...args) {
     let self = this;
-    let server = http.createServer((req, res) => {
-      let ctx = { req, res };
-      // koa2.0和3.0的原理
-      // next(0);
-      // function next(idx) {
-      //   if (idx >= self.middleware.length) return;
-      //   self.middleware[idx](ctx, () => next(idx + 1));
-      // }
-      // 另一种写法 koa1.0 的原理
-      self.middleware.reduceRight(
-        (val, item) => {
-          return item.bind(null, ctx, val);
-        },
-        function() {}
-      )();
-    });
+    let server = http.createServer(this.callback);
     server.listen(...args);
   }
 }
