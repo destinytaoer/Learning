d4e33ce066f2abd58c8d9e2e7efe1c58528d60ee
destinytaoer <1848765519@qq.com>
2019-11-24 16:38:10 +0800

    [vue&React] 添加 Vue scope-slot 学习

---
+++ Vue&React/Vue/scope-slot/demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <List :data="data">
+      <template v-slot="{item}">
+        <span>{{item}}</span>
+      </template>
+    </List>
+  </div>
+</template>
+<script>
+import List from "./list";
+export default {
+  data() {
+    return { data: ["苹果", "香蕉", "橘子"] };
+  },
+  components: {
+    List
+  }
+};
+</script>
---
+++ Vue&React/Vue/scope-slot/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div
+      v-for="(item,key) in data"
+      :key="key"
+    >
+      <slot :item="item"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
