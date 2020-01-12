aff57619d77f8ae4f77986a041d48074fe4f9aba
destinytaoer <1848765519@qq.com>
2020-01-12 17:29:43 +0800

    [React] 学习 redux 中间件

--- React/demo/src/store/actions/counter.js
+++ React/demo/src/store/actions/counter.js
@@ -1,10 +1,28 @@
 import * as Types from '../actionTypes'
 
 function add(count) {
-  return {type: Types.INCREMENT, count}
+  return function (dispatch, getState) {
+    setTimeout(() => {
+      dispatch({type: Types.INCREMENT, count})
+    }, 1000);
+  }
+  // return {type: Types.INCREMENT, count}
 }
 function minus(count) {
-  return {type: Types.DECREMENT, count}
+  // return {type: Types.DECREMENT, count}
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve({type: Types.DECREMENT, count})
+  //   }, 1000);
+  // })
+  return {
+    type: Types.DECREMENT,
+    payload: new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(100)
+      }, 1000);
+    })
+  }
 }
 
 export {add, minus}
--- React/demo/src/store/index.js
+++ React/demo/src/store/index.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware} from 'redux'
 import reducer from './reducers'
-
-export default createStore(reducer)
+import reduxLogger from 'redux-logger'
+import reduxThunk from 'redux-thunk'
+import reduxPromise from 'redux-promise'
+export default createStore(reducer, applyMiddleware(reduxLogger, reduxThunk, reduxPromise))
