3750db496136020b764fa8415d1822cff7c4ace1
destinytaoer <1848765519@qq.com>
2020-01-12 22:09:57 +0800

    [React] 应用 redux-devtools

--- React/demo/src/store/index.js
+++ React/demo/src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware, compose} from 'redux'
 import reducer from './reducers'
 import reduxLogger from 'redux-logger'
 import reduxThunk from 'redux-thunk'
 import reduxPromise from 'redux-promise'
-export default createStore(reducer, applyMiddleware(reduxLogger, reduxThunk, reduxPromise))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENDTION_COMPOSE__ || compose;
+export default createStore(reducer, composeEnhancers(applyMiddleware(reduxLogger, reduxThunk, reduxPromise)));
