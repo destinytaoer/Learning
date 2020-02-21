74d27cda9be304012fee9e5a2609854e0712ac23
destinytaoer <1848765519@qq.com>
2020-02-21 22:50:40 +0800

    [Webpack] 学习基础配置

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -1,1 +1,2 @@
-console.log('hello webpack');
+import str from './a.js';
+console.log(str);
---
+++ Webpack/webpack.config.js
@@ -0,0 +1,13 @@
+// webpack 是基于 Node 的
+let path = require('path');
+module.exports = {
+  // 模式, 默认有两种: production/development
+  mode: 'development',
+  entry: './src/index.js', // 入口
+  output: {
+    // 打包后的文件名
+    filename: 'bundle.js',
+    // 路径必须是绝对路径
+    path: path.resolve(__dirname, 'dist')
+  }
+};
