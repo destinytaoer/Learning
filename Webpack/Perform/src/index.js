ec9035c7e0eeccab630d343f7d11c75f5611fc3a
destinytaoer <1848765519@qq.com>
2020-02-26 15:21:51 +0800

    [Webpack] 构建 webpack 并学习 noParse 优化

---
+++ Webpack/Perform/src/index.js
@@ -0,0 +1,3 @@
+console.log('a');
+
+import $ from 'jquery';
---
+++ Webpack/Perform/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    // 配置不需要 webpack 解析依赖关系的模块, 因为 jquery 是独立的包, 不存在其它依赖
+    noParse: /jquery/,
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
