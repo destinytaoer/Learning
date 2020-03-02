67ab580031a006649ac829e9227050cfa692fe63
destinytaoer <1848765519@qq.com>
2020-03-02 17:51:11 +0800

    [webpack] 动态导入

--- Webpack/Perform/src/index.js
+++ Webpack/Perform/src/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<div>hello</div>, window.root);
+function handleClick(e) {
+  return import('./source').then(data => {
+    console.log(data);
+  });
+}
+
+render(
+  <div>
+    <button onClick={handleClick}>hello</button>
+  </div>,
+  window.root
+);
---
+++ Webpack/Perform/src/source.js
@@ -0,0 +1,3 @@
+// export default 'aa';
+let str = 'a';
+export { str };
