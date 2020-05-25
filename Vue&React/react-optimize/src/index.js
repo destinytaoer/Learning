c79d9d9dd609208af11af0b8cef42a54dbf65b9d
destinytaoer <1848765519@qq.com>
2020-05-25 11:51:20 +0800

    [React] 性能优化 PureComponent
    1. PureComponent 的使用
    2. PureComponent 原理

--- Vue&React/react-optimize/src/index.js
+++ Vue&React/react-optimize/src/index.js
@@ -50,4 +50,64 @@ import ReactDOM from 'react-dom';
 //   }
 // }
 
-ReactDOM.render(<Table data={data} />, document.getElementById('root'));
+/* React.PureComponent */
+class PureComponent extends React.Component {
+  shouldComponentUpdate(newProps) {
+    // 判断组件是否需要更新
+    return !shallowEqual(this.props, newProps);
+  }
+}
+// 浅比较, 只比较一层
+// 相等则返回 true, 否则返回 false
+function shallowEqual(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (typeof obj1 != 'object' || obj1 == null || typeof obj2 != 'object' || obj2 == null) {
+    return false;
+  }
+
+  // 都是对象,且不是同一个对象
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    if (!obj2.hasOwnProperty(key) || obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
+}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.inputRef = React.createRef();
+  }
+  state = {
+    counter: { number: 0 },
+  };
+  add = (e) => {
+    let oldState = this.state;
+    let amount = parseInt(this.inputRef.current.value);
+    let newState = {
+      ...oldState,
+      counter: amount === 0 ? oldState.counter : { number: oldState.counter.number + amount },
+    };
+    this.setState(newState);
+  };
+  render() {
+    return (
+      <div>
+        <Counter counter={this.state.counter} />
+        <input type='text' ref={this.inputRef} />
+        <button onClick={this.add}>+</button>
+      </div>
+    );
+  }
+}
+class Counter extends React.PureComponent {
+  render() {
+    console.log('render');
+    return <div>{this.props.counter.number}</div>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
